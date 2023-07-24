ce7d3894732968691c8d730710b182bd9854b271
zr2x <master5212345@gmail.com>
2023-07-24 17:57:09 +0500

    Initial Commit

---
+++ GCDTest/SecondVC.swift
@@ -0,0 +1,17 @@
+//
+//  SecondVC.swift
+//  GCDTest
+//
+//  Created by Искандер Ситдиков on 22.07.2023.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GCDTest/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  GCDTest
+//
+//  Created by Искандер Ситдиков on 22.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //    lazy var textButton : UIButton = {
+    //       let button = UIButton()
+    //        button.setTitle("Press me", for: .normal)
+    //        button.backgroundColor = .cyan
+    //        button.setTitleColor(.darkGray, for: .highlighted)
+    //        button.frame = CGRect(x: 0, y: 0, width: 200, height: 40)
+    //
+    //        return button
+    //
+    //    }()
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        
+        //        textButton.addTarget(self, action: #selector(buttoAction), for: .touchUpInside)
+    }
+    
+    
+    //    override func viewDidAppear(_ animated: Bool) {
+    //        super.viewDidAppear(animated)
+    //        setupViews()
+    //        setupConstraints()
+    //        self.title = "vc 1"
+    //    }
+    
+    //    func setupViews() {
+    //        view.addSubview(textButton)
+    //        textButton.translatesAutoresizingMaskIntoConstraints = false
+    //    }
+    //
+    //    func setupConstraints() {
+    //      textButton.translatesAutoresizingMaskIntoConstraints = false
+    //        textButton.center = view.center
+    //    }
+    //
+    //    @objc func buttoAction() {
+    //        let secondVC = SecondVC()
+    //        navigationController?.pushViewController(secondVC, animated: true)
+    
+}
+
